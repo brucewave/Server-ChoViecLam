3709141b4bc5c93311852964fd763cf8616e2cbd
brucewave <longthanh.dev@gmail.com>
2024-10-13 02:24:16 +0700

    setup server

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+
+app.use(cors());
+const PORT = 3001;
+
+app.get("/auth/hello", (_req, res) => {
+    res.send("Hello World");
+});
+
+app.listen(PORT, (err) => {
+    if(err){
+        console.log(err)
+        return;
+    }
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
