36ee6688a1229c42c670ef0332be755606fbe0af
brucewave <longthanh.dev@gmail.com>
2024-10-14 23:32:46 +0700

    done login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -51,6 +51,38 @@ const register = asyncHandle(async (req, res) => {
 	});
 });	
 
+const login = asyncHandle(async (req, res) => {
+	const {email, password} = req.body;
+
+	const existingUser = await userModel.findOne({email});
+
+	if (!existingUser) {
+		res.status(403).json({
+			message: 'Tài khoản không tồn tại',
+		});
+		throw new Error('Tài khoản không tồn tại');
+	}
+	
+	const isMatchPassword = await bcrypt.compare(password, existingUser.password);
+
+	if(!isMatchPassword){
+		res.status(401);
+		throw new Error('Email hoặc mật khẩu không đúng');
+	}
+
+	res.status(200).json({
+		message: 'Đăng nhập thành công',
+		data: {
+			email: existingUser.email,
+			id: existingUser.id,
+			accesstoken: await getJsonWebToken(email, existingUser.id),
+		},
+	});
+});
+
+
+
 module.exports = {
 	register,
+	login,
 };
