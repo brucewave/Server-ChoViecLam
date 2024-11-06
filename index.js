9be4d114c3f461ab49e548955249062656fbe80c
brucewave <longthanh.dev@gmail.com>
2024-11-06 12:08:14 +0700

    add api get all user

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const authRouter = require('./src/routers/authRouter');
 const userRouter = require('./src/routers/userRouter');
+const verifyToken = require('./src/middlewares/verifyMiddleware');
 const connectDB = require('./src/configs/connectDb');
 const errorMiddleHandle = require('./src/middlewares/errorMiddleware');
 const app = express();
@@ -19,7 +20,8 @@ app.use(express.json());
 const PORT = 3001;
 
 app.use('/auth', authRouter);
-app.use('/users', userRouter);
+
+app.use('/users', verifyToken, userRouter);
 // app.use('/users', verifyToken, userRouter);
 // app.use('/events', verifyToken, eventRouter);
 
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,8 +1,23 @@
 const asyncHandler = require('express-async-handler');
+const UserModel = require('../models/userModel');
 
 const getAllUsers = asyncHandler(async (req, res) => {
-    console.log('ttetet');
-    res.status(200).json({message: 'Hello World'});
+    const users = await UserModel.find({});
+
+    
+    const data = [];
+    users.forEach((user) => {
+        data.push({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+        });
+    });
+
+    res.status(200).json({
+        message: 'Get all users successfully',
+        data,
+    });
 });
 
 module.exports = {
